0806803ac1ee5a40f51af5fc6e8c3580b228d678
haagmin5382 <itworker5382@gmail.com>
2022-09-17 19:44:45 +0900

    :lipstick: style : switch header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
-
 const HeaderContainer = styled.header`
   text-align: center;
 `;
+
 function Header() {
   return (
     <HeaderContainer>
-      <h1>Title</h1>
       <Nav />
     </HeaderContainer>
   );
